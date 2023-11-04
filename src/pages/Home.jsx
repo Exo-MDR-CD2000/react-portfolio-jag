9754b2a88d439f68bcf225467931776c03124d56
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-03 20:32:21 -0700

    added some styles to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import image from "../assets/new-exotic-bird.jpg";
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Hi, my name is Jose Guillen and welcome to my portfolio.</h1>
-            <p>
-            Don&rsquo;t mind the cool exotic bird up top.
-            </p>
+  return (
+    <div className="hero-container-box">
+      <div className="hero-image">
+        <div className="hero-avatar">
+            <img src= {image} alt="exotic bird" />
+          <h1>Hi, my name is Jose Guillen and welcome to my portfolio.</h1>
+          <p>Don&rsquo;t mind the cool exotic bird up top.</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
