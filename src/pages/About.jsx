4913ecfd8bd2f961e66ba1e3e6067e957865fc88
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-07 14:55:37 -0700

    about page classes added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,18 @@
 export default function About() {
-    return (
-      <div>
+  return (
+    <div className="about-page">
+      <div className="about-h1">
         <h1>About Page</h1>
+      </div>
+      <div className="about-p">
         <p>
-          An aspiring web developer based in the valley, also known as Arizona. You can view some of the recent projects by clicking on the projects tab.
-          My resume is also available for download on the resume tab. If you have any questions or would like to get in contact with me, 
-          please feel free to send me a message on the contact tab.
+          An aspiring web developer based in the valley, also known as Arizona.
+          You can view some of the recent projects by clicking on the projects
+          tab. My resume is also available for download on the resume tab. If
+          you have any questions or would like to get in contact with me, please
+          feel free to send me a message on the contact tab.
         </p>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
