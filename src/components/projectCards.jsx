bdc264120a9f32ddeadc87199feee4d69a4c6cd2
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-07 15:10:17 -0700

    project cards template

--- src/components/projectCards.jsx
+++ src/components/projectCards.jsx
@@ -1,1 +1,13 @@
-// import project cards as templates here
+function ProjectCards({ title, imageUrl, description, projectUrl }) {
+    return (
+      <div className="card" role="button">
+        <h3>{title}</h3>
+        <a href={projectUrl}>
+          <img src={imageUrl} alt={title} />
+        </a>
+        <p>{description}</p>
+      </div>
+    );
+  }
+  
+  export default ProjectCards;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,26 @@
-export default function Projects() {
-    return (
-      <div>
-        <h1>Projects</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
-  }
-  
+import ProjectCards from "../components/ProjectCards";
+
+
+function Projects() {
+  return (
+    <section className="portfolio-cards-container" id="portfolio">
+      <section className="portfolio-h2">
+        <h2>Portfolio</h2>
+      </section>
+      <section className="portfolio-p">
+        <p>Here are some of my projects. Most are placeholder boxes until further notice.</p>
+      </section>
+      <section className="portfolio-cards">
+        <ProjectCards
+          title="HTML Symantecs"
+          imageUrl="./assets/images/html-symantecs-mockup.png"
+          description="Refactored code using HTML semantecs"
+          projectUrl="https://github.com/Exo-MDR-CD2000/week-1-challenge-HTML-symantecs"
+        />
+        {/* Add more ProjectCard components here */}
+      </section>
+    </section>
+  );
+}
+
+export default Projects;
