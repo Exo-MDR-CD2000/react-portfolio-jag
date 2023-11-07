1b2773cf905c93850a7878b7c99f89e8320234cc
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-07 16:45:40 -0700

    bootstrap styling for contact form

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Bootstrap for future use
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,5 @@
 import { useState } from "react";
 
-// export function ContactBlurb() {
-//   <div className="contact-blurb">
-//     <h1>Contact</h1>
-//     <p>
-//       If you have any questions or would like to get in contact with me, 
-//       please feel free to send me a message here.
-//     </p>
-//   </div>
-// }
-
-
 export default function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -26,23 +15,45 @@ export default function ContactForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Message:
-        <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-      </label>
-      <input type="submit" value="Submit" />
+    <form onSubmit={handleSubmit} className="container mt-5">
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message:</label>
+        <textarea className="form-control" id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   );
 }
 
 
 // look into this site for email form: https://www.emailjs.com/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function ContactBlurb() {
+//   <div className="contact-blurb">
+//     <h1>Contact</h1>
+//     <p>
+//       If you have any questions or would like to get in contact with me, 
+//       please feel free to send me a message here.
+//     </p>
+//   </div>
+// }
